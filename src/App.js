f58b8f3a7366144b0c05795cb29e42c5bd4e1d19
Caspar Velsen-Zerweck <casparvelsen-zerweck@MBP-von-Caspar.fritz.box>
2022-06-21 09:30:28 +0200

    Remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-import Title from "./modules/Title";
-import Paragraph from "./modules/Paragraph";
-import styled from "styled-components";
-import Button from "./modules/Button";
-
 function App() {
   return <main>Hofladen Loburg</main>;
 }
-
-const Test = styled.div`
-  border: 0.5px solid grey;
-`;
 
 export default App;
