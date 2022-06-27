2a1f048a69bc02de2ebf47288eec97006fffbf8c
caspar_velsen <caspar@velsen-zerweck.de>
2022-06-27 21:32:52 +0200

    Slight changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,9 @@ export default function Footer() {
         <div className={styles.logo}>Logo</div>
         <div className={styles.information}>
           <div className={styles.row}>
-            <Title level={3}>Hofladen</Title>
+            <Title level={3} titleStyle="light">
+              Hofladen
+            </Title>
             <ul className={styles.list}>
               <li>Straße, Hausn</li>
               <li>Postleitzahl, Ort</li>
@@ -26,23 +28,29 @@ export default function Footer() {
             </ul>
           </div>
           <div className={styles.row}>
-            <Title level={3}>Öffnungszeiten</Title>
-            <ul className="list">
+            <Title level={3} titleStyle="light">
+              Öffnungszeiten
+            </Title>
+            <ul className={styles.list}>
               <li>Mo-Fr 10-18 Uhr</li>
               <li>Sa & So 10-14 Uhr </li>
             </ul>
           </div>
           <div className={styles.row}>
-            <Title level={3}>Rechtliches</Title>
-            <ul className="list">
+            <Title level={3} titleStyle="light">
+              Rechtliches
+            </Title>
+            <ul className={styles.list}>
               <li>Impressum</li>
               <li>Datenschutz</li>
               <li>AGB</li>
             </ul>
           </div>
           <div className={styles.row}>
-            <Title level={3}>Folge uns</Title>
-            <ul className="list">
+            <Title level={3} titleStyle="light">
+              Folge uns
+            </Title>
+            <ul className={styles.list}>
               <li>Instagram</li>
               <li>Facebook</li>
               <li>...</li>
@@ -52,7 +60,7 @@ export default function Footer() {
       </div>
       <Divider color="normal" size="large" />
       <div classNameName={styles.stamp}>
-        <Paragraph textColor="light" textStyle="small">
+        <Paragraph textColor="gray" textStyle="small">
           &copy;{new Date().getFullYear()} HOFLADEN LOBURG | All right reserved
           | Terms of service | Privacy |
         </Paragraph>
