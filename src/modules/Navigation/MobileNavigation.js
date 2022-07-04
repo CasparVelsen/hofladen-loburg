0ea40c04d6acd9ec975af3adac3aa69e6be47a45
caspar_velsen <caspar@velsen-zerweck.de>
2022-07-04 16:42:26 +0200

    Add logo to header

--- src/modules/Navigation/MobileNavigation.js
+++ src/modules/Navigation/MobileNavigation.js
@@ -2,6 +2,8 @@ import Hamburger from "../Hamburger/Hamburger";
 import styles from "./NavBar.module.scss";
 import NavLinks from "./NavLinks";
 import { BsInstagram } from "react-icons/bs";
+import Logo from "../../images/logo-negativ.jpg";
+import { NavLink } from "react-router-dom";
 
 export default function MobileNavigation({ isOpen, toggleMenu }) {
   return (
@@ -11,6 +13,16 @@ export default function MobileNavigation({ isOpen, toggleMenu }) {
         <a href="https://www.instagram.com/" className={styles.socialIcon}>
           <BsInstagram size={25} color="white" />
         </a>
+      )}
+      {isOpen && (
+        <NavLink to="/">
+          <img
+            className={styles.logoMobileNav}
+            src={Logo}
+            height="120px"
+            alt="Logo"
+          />
+        </NavLink>
       )}
       <Hamburger toggleMenu={toggleMenu} active={isOpen} />
     </nav>
--- src/modules/Navigation/NavBar.js
+++ src/modules/Navigation/NavBar.js
@@ -3,6 +3,8 @@ import Navigation from "./Navigation";
 import { useState } from "react";
 import styles from "./NavBar.module.scss";
 import classNames from "classnames";
+import Logo from "../../images/logo-negativ.jpg";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,10 +12,17 @@ export default function NavBar() {
   function toggleMenu() {
     setIsOpen(!isOpen);
   }
+
   return (
     <div className={classNames(isOpen ? styles.navActive : styles.nav)}>
-      <div>Logo</div>
-      <Navigation />
+      {!isOpen && (
+        <NavLink to="/">
+          <div className={styles.logoBox}>
+            <img src={Logo} height="100%" alt="Logo" />
+          </div>
+        </NavLink>
+      )}
+      <Navigation isOpen={isOpen} />
       <MobileNavigation isOpen={isOpen} toggleMenu={toggleMenu} />
     </div>
   );
